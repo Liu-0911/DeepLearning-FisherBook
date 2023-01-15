a77ce3fc39417587b2d69056d2bbe46faae1e01a
Liu <lbx@qq.com>
2023-01-15 15:51:46 +0800

    第一次学习 2023.01.15

--- 第三章 神经网络/Sigmoid函数实现.py
+++ 第三章 神经网络/Sigmoid函数实现.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-
-def step_function(x):
-    return np.array(x > 0,dtype=np.int64)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 def drawdwmo1():
     x = np.arange(-5.0,5.0,0.1)
-    y = step_function(x)
+    y = sigmoid(x)
     plt.plot(x,y)
     plt.ylim(-0.1,1.1)
     plt.show()
